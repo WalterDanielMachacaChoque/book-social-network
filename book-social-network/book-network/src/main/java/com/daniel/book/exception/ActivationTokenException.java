42fa7a0ba6cf7e275c7031dde3497c1f15729ca0
Walter Daniel Machaca Choque <wally8795@gmail.com>
2024-10-12 21:25:25 -0400

    Se Agrega el Proyecto

---
+++ book-social-network/book-network/src/main/java/com/daniel/book/exception/ActivationTokenException.java
@@ -0,0 +1,8 @@
+package com.daniel.book.exception;
+
+public class ActivationTokenException extends RuntimeException{
+
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
---
+++ book-social-network/book-network/src/test/java/com/daniel/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
